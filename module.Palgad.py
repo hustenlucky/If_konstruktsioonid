0db938079a565490857f37d750bdb20dabcd74cc
melon3000 <milan.petrovski8@gmail.com>
2025-04-07 11:40:58 +0300

    1

--- module.Palgad.py
+++ module.Palgad.py
@@ -1,25 +1,26 @@
 def Lisa_andmed(p:list,i:list):
-    """
-    Funktsioon, mis lisab inimese ja tema palga.
+    """ Добавляем несколько людей и зарплат
     """
     while True:
-    try:
-        nimi=input("Nimi: ")
-        if nimi.isalpha():
-            try:
-                palk=float(input("Palk: "))
-            except:
-                print("Palk on arv!")
-            break
-            print("Andmed on lisatud")
-    except:
-        print("Kirjuta ainult tähtede kasutades")
-    p.append(palk)
-    i.append(nimi)
+        try:
+            nimi=input("Nimi: ")
+            if nimi.isalpha():
+                try:
+                    palk=float(input("Palk: "))
+                except:
+                    print("Palk on arv!")
+                break
+                print("Andmed on lisatud!")
+                p.append(palk)
+                i.append(nimi)
+        except:
+            print("Kirjuta ainult tähtede kasutades!")
+
+        
 def Kustuta_andmed(p:list,i:list):
+    """ Удалить человека и его зарплату
     """
-    """
-    try: 
+    try:
         nimi=input("Nimi: ")
         if nimi.isalpha():
             k=i.count(nimi)
@@ -28,29 +29,75 @@ def Kustuta_andmed(p:list,i:list):
                     ind=i.index(nimi)
                     i.pop(ind)
                     p.pop(ind)
-                print("Andmed on kustutatud")
+                print("Andmed on kustutatud!")
             else:
-                print("Andmed puuduvad!")
+                print("Andmed on puuduvad!")
     except:
-        print("Kirjuta ainult tähtede kasutades")
+        print("Kirjuta ainult tähtede kasutades!")
+
 def Suurim_palk(p:list,i:list):
+    """ Самая большая зарплата и кто ее получает
     """
+    max_palk=max(p)
+    print(f"Suurim palk on {max_palk}")
+    k=p.count(max_palk)
+    ind=p.index(max_palk)
+    for j in range(k):
+        ind=p.index(max_palk,ind)
+        print(f"Suurim palk on {max_palk} ja selle saab {i[ind]}")
+        ind=ind+1
+
+def Väiksem_palk(p:list,i:list):
+    """ Самая маленькая зарплата и кто ее получает
     """
-    suurim=max(p)
-    print(f"Suurim palk on {suurim}")
-    k=p.count(suurim)
-    if k>0:
-        for j in range(k):
-            ind=p.index(suurim,ind)
-            print(f"Saab kätte{i[ind]}")
-            ind=ind+1
-def Sorteerimine(p:list,i:list)-> any:
+    min_palk=min(p)
+    print(f"Väiksem palk on {min_palk}")
+    k=p.count(min_palk)
+    ind=p.index(min_palk)
+    for j in range(k):
+        ind=p.index(min_palk,ind)
+        print(f"Väiksem palk on {min_palk} ja selle saab {i[ind]}")
+        ind=ind+1
+
+def sorteerimine_kasvav(p:list,i:list)->any:
+    """ Упорядочить зарплаты в порядке возрастания вместе с именами
+    :param p: Список зарплат
+    :type p: list
+    :param i: Список имён
+    :type i: list
     """
-    """
-    v=input("Vali märk: > (kasvav) või < (kahanev)")#Try except koos while True'ga
     for n in range(0,len(i)):
         for m in range(n,len(i)):
-            if eval(str(p[n])+v+str(p[m])):
-               p[n]>[m]=p[m],p[n]
-               i[n]>[m]=i[m],i[n]
+            if p[n]>p[m]:
+                p[n],p[m]=p[m],p[n]
+                i[n],i[m]=i[m],i[n]
     return p,i
+
+def sorteerimine_kahanev(p:list,i:list)->any:
+    """ Упорядочить зарплаты в порядке убывания вместе с именами
+    :param p: Список зарплат
+    :type p: list
+    :param i: Список имён
+    :type i: list
+    """
+    for n in range(0,len(i)):
+        for m in range(n,len(i)):
+            if p[n]<p[m]:
+                p[n],p[m]=p[m],p[n]
+                i[n],i[m]=i[m],i[n]
+    return p,i
+
+def Võrdsed_palgad(p:list,i:list):
+    """ Узнать, кто получает одинаковую зарплату, найти сколько таких людей вывести их данные на экран
+    """
+    hulk=set(p)
+    print(hulk)
+    for palk in hulk:
+        k=p.count(palk)
+        if k>1:
+            print(f"Palk {palk}")
+            ind=p.index(palk)
+            for j in range(k):
+                ind=p.index(palk,ind)
+                print(f"Saab kätte {i[ind]}")
+                ind+=1
--- palgad.py
+++ palgad.py
@@ -1,21 +1,26 @@
-from module.Palgad import * 
+from module_palgad import *
 
 
 palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
 
 while True:
-    print("Andmed:")
+    print("Andmed: ")
     print(inimesed)
     print(palgad)
-    print("Vajuta:\n1-Andmete lisamiseks\n2-Andmete kustutamiseks nime järgi")
+    print("Vajuta:\n1-Andmete lisamiseks\n2-Andmete kustutamiseks nime järgi\n3-Leida kes saab kätte suurim palk\n4-Leida kes saab kätte kõige väiksem palk ja milline ta on\n5-Järjestada palgad kasvavas järjekorras koos nimedega\n6-Järjestada palgad kahanevas järjekorras koos nimedega")
     v=int(input())
     if v==1:
-        k=int(input("Mitu inimest?"))
-        Lisa_andmed(palgad,inimesed)
+        k=int(input("Kui palju inimesi sa tahad lisa?: "))
+        for i in range(k):
+            Lisa_andmed(palgad,inimesed)
     elif v==2:
         Kustuta_andmed(palgad,inimesed)
     elif v==3:
         Suurim_palk(palgad,inimesed)
     elif v==4:
-        palgad,inimesed=Sorteerimine_kasvav(palgad,inimesed)
+        Väiksem_palk(palgad,inimesed)
+    elif v==5:
+        palgad,inimesed=sorteerimine_kasvav(palgad,inimesed)
+    elif v==6:
+        palgad,inimesed=sorteerimine_kahanev(palgad,inimesed)
