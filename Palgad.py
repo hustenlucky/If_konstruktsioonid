ad7ae9389a43ab19a195b6d26a9a4262448f8aaf
melon3000 <milan.petrovski8@gmail.com>
2025-04-11 11:42:30 +0300

    5.5

--- Palgad.py
+++ Palgad.py
@@ -3,14 +3,14 @@ from module_palgad import *
 
 palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
-
+ 
 while True:
     print("Andmed: ")
     print(inimesed)
     print(palgad)
     print("Vajuta:\n1-Andmete lisamiseks\n2-Andmete kustutamiseks nime järgi\n3-Leida kes saab kätte suurim palk\n4-Leida kes saab kätte kõige väiksem palk ja milline ta on\n5-Järjestada palgad kasvavas järjekorras koos nimedega\n6-Järjestada palgad kahanevas järjekorras koos nimedega\n7-Teada saada, kes saavad võrdset palka, leida, kui palju neid on ja kuvada nende andmed ekraanile\n8-Teha palgaotsing isiku nime järgi\n9-Top vaeseimad ja rikkamad inimesed\n11-Tulumaks")
     v=int(input())
-    if v==1:
+    if v==1:        
         k=int(input("Kui palju inimesi sa tahad lisa?: "))
         for i in range(k):
             Lisa_andmed(palgad,inimesed)
@@ -25,7 +25,7 @@ while True:
         print(palgad,inimesed)
     elif v==6:
         palgad,inimesed=sorteerimine_kahanev(palgad,inimesed)
-        print(palgad,inimesed) 
+        print(palgad,inimesed)   
     elif v==7:
         Võrdsed_palgad(palgad,inimesed) 
     elif v==8:   
