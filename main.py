d48daff2ec8d55bd6161fe3cb55150511bc4404e
melon3000 <milan.petrovski8@gmail.com>
2025-04-25 11:07:01 +0300

    1

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-from sonastik import *
-print("Tere tulemast kolme keele sõnastikku!")
-sonad = loo_sonastik()
+# Tühja sõnastiku loomine
+andmed = {}
+# Võtmete ja väärtustega
+andmed = {'nimi': 'Mari', 'vanus': 25, 'keel': 'eesti'}
+# dict() funktsiooniga
+andmed = dict(nimi='Mari', vanus=25, keel='eesti')
+print(andmed)
+print(andmed["nimi"]) #показать значение ключа "nimi"
+print(andmed.get("nimi")) #показать значение ключа "nimi"
+print(andmed.get("nimi_","Ei ole sõnastikus")) 
+print(andmed.keys()) #показать ключи
+print(andmed.values()) #показать значения
+for k, v in andmed.items(): #показать список ключ и значение
+    print(k, v)
+andmed["email"]="ananas@ananas.ee" #добавление ключа и значения
+print(andmed)
+andmed["prillid"]=True #добавление ключа
+print(andmed)
+del andmed['prillid'] #удаление ключа
+print(andmed)
+andmed.update({'nimi':'Kati'}) #Обновление значения имя
+print(andmed)
+
+from random import *
+read = ['Mis on Python?-programmeerimiskeel', 'Eesti pealinn?-Tallinn', 'Mis on eesti rahvuslill?-Rukkilill', 'Mis on eesti rahvuslind?-Suitsupääsuke']
+kus_vas = {}
+for rida in read:
+    kysimus, vastus = rida.split('-')
+    kus_vas[kysimus.strip()] = vastus.strip()
+print(kus_vas) # {'Mis on Python?': 'programmeerimiskeel', 'Eesti pealinn?': 'Tallinn', 'Mis on eesti rahvuslill?': 'Rukkilill', 'Mis on eesti rahvuslind?': 'Suitsipääsuke'}
+print(kus_vas['Mis on Python?'])
+
+kysimused=list(kus_vas.keys())
 while True:
-    print("\nMenu:")
-    print("1 - Tõlgi sõna\n2 - Lisa uus sõna \n3 - Paranda sõna\n4 - Kuva kõik sõnad\n5 - Testi teadmisi\n0 - Välja")
-    valik = input("\nSisesta valik: ")
-    if valik == "1":
-        allikas, siht=vali_keelte_suund()
-        sona=input(f"Sisesta sõna({allikas})")
-        print(f"Tõlge: {tolkija(sonad,allikas, siht, sona)}")   
-    elif valik == "2":
-         lisa_sona(sonad)
-    elif valik == "3":
-        paranda_sona(sonad)     
-    elif valik == "4":
-        kuva_sonad(sonad)    
-    elif valik == "5":
-        testi_teadmisi(sonad)       
-    elif valik == "0":
+    n=randint(0, len(read)-1)
+    valitud_kysimus=kysimused[n]
+    print(valitud_kysimus)
+    vastus=input("Sisesta vastus: ")
+    if kus_vas[valitud_kysimus].lower()==vastus.lower():
+        print("Õige vastus")
+    elif kus_vas[valitud_kysimus].lower()==0:
         break
     else:
-        print("Viga! Proovi veel kord.")
+        print("Vale vastus")
